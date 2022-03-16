ff829d449ee20a84110fb1e89c53b64d7579bab1
Leon Sering <leon@sering.eu>
2022-03-16 15:37:13 +0100

    [LocalSearch] Minimizer now also works in parallel.

--- src/solver/local_search/local_improver.rs
+++ src/solver/local_search/local_improver.rs
@@ -33,87 +33,44 @@ impl<F: SwapFactory> Minimizer<F> {
 
 impl<F: SwapFactory> LocalImprover for Minimizer<F> {
     fn improve(&self, schedule: &Schedule) -> Option<Schedule> {
-        let swap_iterator = self.swap_factory.create_swap_iterator(schedule);
-        let mut counter = 0;
+        let swap_iterator = self.swap_factory.create_swap_iterator(schedule).par_bridge();
         let (best_objective_value, best_schedule) =
-            swap_iterator.filter_map(|swap| {counter += 1; swap.apply(schedule).ok()})
+            swap_iterator.filter_map(|swap| swap.apply(schedule).ok())
             .map(|sched| (sched.objective_value(),sched))
             .min_by(|(o1,_), (o2,_)| o1.partial_cmp(o2).unwrap()).unwrap();
 
         if best_objective_value < schedule.objective_value() {
-            println!("Improvement found after {} swaps.", counter);
             Some(best_schedule)
         } else {
-            println!("No improvement found after {} swaps.", counter);
             None
         }
     }
 }
 
 
-
 ///////////////////////////////////////////////////////////
-////////////////////// TakeFirst //////////////////////////
-///////////////////////////////////////////////////////////
-
-
-pub(crate) struct TakeFirst<F: SwapFactory + Send> {
-    swap_factory: F,
-}
-
-
-impl<F: SwapFactory + Send> TakeFirst<F> {
-    pub(crate) fn new(swap_factory: F) -> TakeFirst<F> {
-        TakeFirst{swap_factory}
-    }
-}
-
-impl<F: SwapFactory + Send> LocalImprover for TakeFirst<F> {
-    fn improve(&self, schedule: &Schedule) -> Option<Schedule> {
-        let old_objective = schedule.objective_value();
-
-        let swap_iterator = self.swap_factory.create_swap_iterator(schedule);
-
-        // swap_iterator.filter_map(|swap| swap.apply(schedule).ok())
-            // .find(|sched| sched.objective_value() < old_objective)
-        let mut counter = 0;
-        let result = swap_iterator.filter_map(|swap| {counter += 1; swap.apply(schedule).ok()})
-            .find(|sched| sched.objective_value() < old_objective);
-        if result.is_none() {
-            println!("No improvement found after {} swaps.", counter);
-        } else {
-            println!("Improvement found after {} swaps.", counter);
-
-        }
-
-        result
-
-    }
-}
-
-///////////////////////////////////////////////////////////
-///////////////// TakeFirstRecursion //////////////////////
+/////////////////// TakeAnyRecursion //////////////////////
 ///////////////////////////////////////////////////////////
 
 
-pub(crate) struct TakeFirstRecursion<F: SwapFactory + Send + Sync> {
+pub(crate) struct TakeAnyRecursion<F: SwapFactory + Send + Sync> {
     swap_factory: F,
     recursion_depth: u8,
     recursion_width: Option<usize>, // number of schedule that are considered for recursion (the one with best value are taken)
 }
 
 
-impl<F: SwapFactory + Send + Sync> LocalImprover for TakeFirstRecursion<F> {
+impl<F: SwapFactory + Send + Sync> LocalImprover for TakeAnyRecursion<F> {
     fn improve(&self, schedule: &Schedule) -> Option<Schedule> {
         let old_objective = schedule.objective_value();
         self.improve_recursion(vec!(schedule.clone()), old_objective, self.recursion_depth)
     }
 }
 
-impl<F: SwapFactory + Send + Sync> TakeFirstRecursion<F> {
+impl<F: SwapFactory + Send + Sync> TakeAnyRecursion<F> {
 
-    pub(crate) fn new(swap_factory: F, recursion_depth: u8, recursion_width: Option<usize>) -> TakeFirstRecursion<F> {
-        TakeFirstRecursion{swap_factory, recursion_depth, recursion_width}
+    pub(crate) fn new(swap_factory: F, recursion_depth: u8, recursion_width: Option<usize>) -> TakeAnyRecursion<F> {
+        TakeAnyRecursion{swap_factory, recursion_depth, recursion_width}
     }
 
 
