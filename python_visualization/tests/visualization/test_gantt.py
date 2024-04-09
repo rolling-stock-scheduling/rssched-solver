3dd818a09391f39a629ca59ed057f9acebd568e6
u228298 <merlin.unterfinger@sbb.ch>
2024-04-09 16:05:44 +0200

    Remove python package since it has been moved to its own repository

--- python_visualization/tests/visualization/test_gantt.py
+++
@@ -1,8 +0,0 @@
-from rolling_stock_scheduling.data.access import PkgDataAccess
-from rolling_stock_scheduling.io.reader import import_response
-from rolling_stock_scheduling.visualization.gantt import respone_to_gantt
-
-
-def test_response_to_gant():
-    response = import_response(PkgDataAccess.locate_response())
-    respone_to_gantt(response, "Rolling stock schedule")
